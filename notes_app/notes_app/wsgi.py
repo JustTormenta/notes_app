4208bc5ba5354e90ae17ac50a06081b8b4d4bc33
JustTormenta <pavlomirov@gmail.com>
2025-05-11 00:07:13 +0300

    Add func hello world

--- notes_app/notes_app/wsgi.py
+++ notes_app/notes_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for L38_hw project.
+WSGI config for notes_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'L38_hw.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notes_app.settings')
 
 application = get_wsgi_application()
